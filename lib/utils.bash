1f685ebef7d2e25c56cabd2ff47abdd60e46754d
Guruprasad Kulkarni <comdotlinux@users.noreply.github.com>
2021-04-07 08:11:30 +0000

    Added ki installation

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-
+set -x
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ki.
 GH_REPO="https://github.com/Kotlin/kotlin-interactive-shell"
+DOWNLOAD_BASE_URL="https://repo1.maven.org/maven2/org/jetbrains/kotlinx/ki-shell"
 TOOL_NAME="ki"
 TOOL_TEST="echo ':q' | ./bin/ki.sh -v 2> /dev/null | head -1"
 
@@ -14,26 +15,13 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if ki is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if ki has other means of determining installable versions.
-  list_github_tags
+  curl "${curl_opts[@]}" "${DOWNLOAD_BASE_URL}/maven-metadata.xml" 2>/dev/null | grep 'version>' | sed -e 's,.*<version>\([^<]*\)</version>.*,\1,g'
 }
 
 download_release() {
@@ -41,11 +29,11 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for ki
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$DOWNLOAD_BASE_URL/${version}/ki-shell-${version}-archive.zip"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  curl "${curl_opts[@]}" -o "${filename}.sha1" -C - "${url}.sha1" || fail "Could not download $url"
 }
 
 install_version() {
@@ -57,22 +45,20 @@ install_version() {
     fail "asdf-$TOOL_NAME supports release installs only"
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
-  local release_file="$install_path/$TOOL_NAME-$version.tar.gz"
+  local release_file="$install_path/$TOOL_NAME-$version.zip"
   (
     mkdir -p "$install_path"
     download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
+    cd "$install_path" && unzip "$release_file" && mv ki/bin . && mv ki/lib . && chmod u+x ./bin/ki.sh || fail "Could not extract $release_file"
     rm "$release_file"
 
-    # TODO: Asert ki executable exists.
     local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    tool_cmd="ki.sh"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
-    rm -rf "$install_path"
+    rm -rvf "$install_path"
     fail "An error ocurred while installing $TOOL_NAME $version."
   )
 }
